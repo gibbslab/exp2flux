e683a887cf20e292a90a06ca1a73e9411e6d460d
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-08-31 22:40:47 -0500

    Update exp2flux

--- R/exp2flux.R
+++ R/exp2flux.R
@@ -1,5 +1,36 @@
 #' @export exp2flux
-exp2flux <- function(model,expression,missing="max",scale=FALSE){
+#' @importFrom sybil findExchReact
+#' @author Kelly Botero <kjboteroo@unal.edu.co> - 
+#' Maintainer: Daniel Camilo Osorio Hurtado <dcosorioh@unal.edu.co>
+#' @title Convert Gene Expression Data to FBA fluxes
+#' @param model A valid model for the \code{'sybil'} package
+#' @param expression A valid ExpressionSet object (one by treatment)
+#' @param missing A character string specifying the value to be used in missing cases; must be one of \code{'gmean'}, \code{'min'}, \code{'1q'}, \code{'mean'}, \code{'median'}, \code{'3q'}, or \code{'max'}
+#' @param scale A boolean value to specify if data must be scaled
+#' @examples 
+#' \dontrun{
+#' # Required Libraries
+#' library("sybil")
+#' library("Biobase")
+#' 
+#' # Reference Data
+#' data("Ec_core")
+#' optimizeProb(Ec_core)
+#' 
+#' # Random Data
+#' gE <- ExpressionSet(matrix(runif(50,1,100),dimnames = list(sample(Ec_core@allGenes,50),c())))
+#' 
+#' # Applying the function
+#' modifiedModel <- exp2flux(model = Ec_core,expression = gE,missing = "gmean",scale = TRUE)
+#' 
+#' # Result
+#' optimizeProb(modifiedModel)
+#' }
+exp2flux <- function(model,expression,missing="gmean",scale=FALSE){
+  # This function was written by Paul McMurdie @ http://stackoverflow.com/questions/2602583/geometric-mean-is-there-a-built-in
+  gm_mean = function(x, na.rm=TRUE){
+    exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+  }
   gpr.expression <- function(gpr,expression,missing){
     gpr <- gsub("[()]","",gpr)
     gpr <- gsub("[[:space:]]","",gpr)
@@ -8,24 +39,31 @@ exp2flux <- function(model,expression,missing="max",scale=FALSE){
     min.complex <- lapply(genes, function(gene){
       lapply(gene, function(gene){
         gExp <- rowMeans(expression@assayData$exprs,na.rm = TRUE)[unlist(gene)]
-        if(identical(rep(TRUE,length(gExp)),as.vector(is.na(gExp)))){
+        if(all(is.na(gExp))){
           gExp <- 0
         }
-        min(gExp,na.rm = TRUE)})
+        return(min(gExp,na.rm = TRUE))})
     })
     exp <- unlist(lapply(min.complex, function(min.complex){sum(unlist(min.complex),na.rm = TRUE)}))
-    exp[exp==0]<-NA
-    if(scale==TRUE){
-      exp <- round((exp/max(exp,na.rm = TRUE)),6)*1000
+    exp[exp==0] <- NA
+    if(missing == "gmean"){
+     exp[is.na(exp)] <- gm_mean(exp,na.rm = TRUE)
+    } else {
+      exp[is.na(exp)] <- summary(exp)[match(missing,c("min","1q","median","mean","3q","max"))]
     }
-    exp[is.na(exp)] <- as.vector(summary(exp)[match(missing,c("min","1q","median","mean","3q","max"))])
     return(exp)
   }
   exp <- gpr.expression(model@gpr,expression,missing=missing)
-  exp[model@react_id%in%findExchReact(model)@react_id] <- abs(model@lowbnd[model@react_id%in%findExchReact(model)@react_id])
+  if(scale==TRUE){
+    exp <- round((exp/max(exp,na.rm = TRUE)),6)*1000
+  }
+  lb <- model@lowbnd
+  ub <- model@uppbnd
   model@lowbnd <- -1*exp
   model@lowbnd[!model@react_rev] <- 0
   model@uppbnd <- exp
+  model@lowbnd[model@react_id%in%findExchReact(model)@react_id] <- lb[model@react_id%in%findExchReact(model)@react_id]
+  model@uppbnd[model@react_id%in%findExchReact(model)@react_id] <- ub[model@react_id%in%findExchReact(model)@react_id]
   return(model)
 }
 
