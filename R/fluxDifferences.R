228cba3207e335261f8ed173c3ba679026e14036
Daniel Osorio <danielcamiloosorio@gmail.com>
2016-09-06 10:17:35 -0500

    Update exp2flux

--- R/fluxDifferences.R
+++ R/fluxDifferences.R
@@ -2,14 +2,14 @@
 #' @importFrom sybil getFluxDist optimizeProb
 #' @author Daniel Camilo Osorio Hurtado <dcosorioh@unal.edu.co>
 #' @title Report the fold change of fluxes between two models
-fluxDifferences <- function(model1,model2,fold2report=2){
+fluxDifferences <- function(model1,model2,foldReport=2){
   f_m1 <- getFluxDist(optimizeProb(model1))
   f_m2 <- getFluxDist(optimizeProb(model2))
   if(identical(length(f_m1),length(f_m2))){
-    fold <- (f_m2/f_m1)-1
+    fold <- ((f_m2-f_m1)/f_m1)
     fold[is.na(fold)] <- 0
     fold[is.infinite(fold)] <- 0
-    different <- (abs(fold)>fold2report)
+    different <- (abs(fold)>foldReport)
     different <-as.data.frame.array(cbind(model1@react_id[different],f_m1[different],f_m2[different],round(fold[different],2)))
     colnames(different) <- c("Reaction","fluxModel1","fluxModel2","foldChange")
     return(as.data.frame(different))
