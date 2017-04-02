c47d21b3adb0448af767c61eef89773f701a93ac
Daniel Osorio <dcosorioh@unal.edu.co>
2017-04-02 17:07:02 -0500

    gprExpression internal function was added

---
+++ R/gprExpression.R
@@ -0,0 +1,16 @@
+gprExpression <- function(gpr,expression,missing){
+  # Split GPR
+  gpr <- splitGPR(GPR = gpr)
+  
+  # Setting the min by complex
+  complexExpression <- complexMin(gprList = gpr, expressionData = expression, organismPathways = organismPathways, missing = missing)
+  
+  # Sum complex values
+  gprExpression <- unlist(lapply(complexExpression, function(complexExpression){sum(unlist(complexExpression),na.rm = TRUE)}))
+  
+  # Replacing empty using the missing option
+  gprExpression[gprExpression == 0] <- summary(object = rowMeans(x = expression@assayData$exprs, na.rm = TRUE))[[match(missing,c("min","1q","median","mean","3q","max"))]]
+  
+  # Return
+  return(gprExpression)
+}
